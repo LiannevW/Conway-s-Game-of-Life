db5a27983cc0a05f6f236a29d59ee9e709bc4559
Lianne van Westrhenen <lianne.van.westrhenen@gmail.com>
2018-01-23 17:30:54 +0100

    basic setup

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Project summary
 
-Work in Progress. Building Conway's Game of Life for practicing React Redux. 
+Work in Progress. Building Conway's Game of Life for practicing React.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Main from './Components/Main'
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<Main />, document.getElementById('root'));
