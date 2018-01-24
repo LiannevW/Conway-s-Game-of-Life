e5df556ddeec67975bf6dbc5d7b3903f98d2c4e7
Lianne van Westrhenen <lianne.van.westrhenen@gmail.com>
2018-01-24 19:02:16 +0100

    grid component

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,14 +1,14 @@
-import React, { PureComponent } from 'react'
-
-class Grid extends PureComponent {
-
-  render() {
-
-    return (
-      <div>
-      </div>
-    )
-  }
-}
-
-export default Grid
+// import React, { PureComponent } from 'react'
+//
+// class Grid extends PureComponent {
+//   render() {
+//
+//     return (
+//       <div>
+//         Grid
+//       </div>
+//     )
+//   }
+// }
+// 
+// export default Grid
--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from 'react-dom';
 import Main from './Components/Main'
 import './index.css';
 
-
 ReactDOM.render(<Main />, document.getElementById('root'));
