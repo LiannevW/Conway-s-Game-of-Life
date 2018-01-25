494e5726c160923aa18770e9a0f267d6a0a78209
Lianne van Westrhenen <lianne.van.westrhenen@gmail.com>
2018-01-25 17:14:53 +0100

    seperated components

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,14 +1,36 @@
-// import React, { PureComponent } from 'react'
-//
-// class Grid extends PureComponent {
-//   render() {
-//
-//     return (
-//       <div>
-//         Grid
-//       </div>
-//     )
-//   }
-// }
-// 
-// export default Grid
+import React, { PureComponent } from 'react'
+import Box from './Box'
+
+class Grid extends PureComponent {
+  render() {
+    const width = this.props.cols * 14
+    var rowsArr = []
+
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; j < this.props.cols; j++) {
+        let boxId = i + "_" + j;
+
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off"
+        rowsArr.push (
+          <Box
+            boxClass = {boxClass}
+            key = {boxId}
+            boxId = {boxId}
+            row = {i}
+            col = {j}
+            selectBox = {this.props.selectBox}
+            />
+        );
+      }
+    }
+
+    return (
+      <div className="grid" style={{width: width}}>
+        {rowsArr}
+      </div>
+    )
+  }
+}
+
+export default Grid
